00948247ecd1bd1e7f847f6214f978c9346023d3
Airin <airinaight@gmail.com>
2021-04-21 17:15:57 +0200

    Add person threshold method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,4 +7,14 @@ public class Calculator {
     public static boolean moreThan100(int a) {
         return a > 100;
     }
+
+    public static String checkPersonThreshold(int numberOfPeople) {
+        String tooManyPeople;
+        if (numberOfPeople > 30) {
+            tooManyPeople = "Zu viele Personen, sofort zurück";
+        } else {
+            tooManyPeople = "Maximale Personenanzahl nicht überschritten";
+        }
+        return tooManyPeople;
+    }
 }
