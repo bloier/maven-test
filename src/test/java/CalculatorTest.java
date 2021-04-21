e0d0740dc47b653c70517554b162ac9cdb000d0a
Airin <airinaight@gmail.com>
2021-04-21 16:23:40 +0200

    Initial Commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculatorTest {
+
+    @Test
+    public void addTest(){
+        //GIVEN
+        int valueOne = 3;
+        int valueTwo = 5;
+
+        //WHEN
+        int sum = Calculator.add(valueOne, valueTwo);
+
+        //THEN
+        assertEquals(8, sum);
+    }
+
+    @Test
+    public void moreThan100Test() {
+        //GIVEN
+        int valueThree = 199;
+
+        //WHEN
+        boolean moreThan100 = Calculator.moreThan100(valueThree);
+
+        //THEN
+        //assertEquals(true, moreThan100);
+        assertTrue(Calculator.moreThan100(valueThree));
+    }
+
+}
